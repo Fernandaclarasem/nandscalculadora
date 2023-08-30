e74cd683455a400d2a8c8648a0da5b9716429aa0
Fernanda Clara <140112342+Fernandaclarasem@users.noreply.github.com>
2023-08-29 23:48:49 -0300

    Add files via upload

---
+++ Calculadora codigo/index.js
@@ -0,0 +1,21 @@
+function adicionar(num) {
+    document.querySelector(".visor").innerHTML += num;
+}
+
+function clean() {
+    document.querySelector(".visor").innerHTML = "";
+}
+
+function back() {
+    let visor = document.querySelector(".visor").innerHTML;
+    document.querySelector(".visor").innerHTML = visor.substring(0; visor.length - 1);
+  }
+
+  function somar() {
+    let visor = document.querySelector(".visor").innerHTML;
+    if(visor) {
+    document.querySelector('.visor').innerHTML = eval(visor)
+    } else {
+        document.querySelector('.visor').innerHTML = "nenhum comando..."
+    }
+  }
